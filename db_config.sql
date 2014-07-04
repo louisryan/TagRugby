b199d5ed5fae79d0cc02acb80dd7bd66e846163c
Louis Ryan <louisryan@me.com>
2014-07-04 21:49:43 +0100

    Database Structure
    Create tables for - teams, fixtures_results, weather, league_table

--- db_config.sql
+++ db_config.sql
@@ -3,3 +3,55 @@
 -- ----------------------------
 DROP TABLE IF EXISTS `teams`;
 CREATE TABLE `teams` (
+	`team_id` int(8) unsigned NOT NULL AUTO_INCREMENT,
+	`team_name` varchar(70) NOT NULL
+	PRIMARY KEY (`team_id`)
+) ENGINE=InnoDB;
+
+-- ----------------------------
+--  Table structure for `fixtures`
+-- ----------------------------
+DROP TABLE IF EXISTS `fixtures_results`;
+CREATE TABLE `fixtures_results` (
+	`fixture_id` int unsigned NOT NULL AUTO_INCREMENT,
+	`phase` varchar(10) 
+	`group` int(5),
+	`datetime` datetime,
+	`team_a_id` int(8) 
+	`team_b_id` int(8)
+	`team_a_score` smallint, 
+	`team_b_score` smallint,
+	PRIMARY KEY (`fixture_id`)
+) ENGINE=InnoDB;
+
+
+-- ----------------------------
+--  Table structure for `weather`
+-- ----------------------------
+DROP TABLE IF EXISTS `weather`;
+CREATE TABLE `weather` (
+	`country` varchar(30),
+	`county` varchar(30),
+	`datetime` datetime,
+	`temp_cel` DOUBLE,
+	`weather` varchar(120),
+) ENGINE=InnoDB;
+
+-- ----------------------------
+--  Table structure for `league_table`
+-- ----------------------------
+DROP TABLE IF EXISTS `league_table`;
+CREATE TABLE `league_table` (
+	`phase` varchar(10) NOT NULL,
+	`division` varchar(10) NOT NULL,
+	`team_id` int(8) NOT NULL,
+	`points` smallint NOT NULL, 
+	`played` smallint NOT NULL, 
+	`win` smallint NOT NULL, 
+	`draw` smallint NOT NULL, 
+	`loss` smallint NOT NULL, 
+	`for` smallint NOT NULL, 
+	`against` smallint NOT NULL, 
+	`diff` smallint NOT NULL,  
+) ENGINE=InnoDB;
+
