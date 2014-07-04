89c9fa94318ccbed437f9e8ad95100c8f9924747
Louis Ryan <louisryan@me.com>
2014-07-04 21:54:34 +0100

    Final DB Structure

--- db_config.sql
+++ db_config.sql
@@ -1,3 +1,4 @@
+
 -- ----------------------------
 --  Table structure for `teams`
 -- ----------------------------
@@ -8,6 +9,7 @@ CREATE TABLE `teams` (
 	PRIMARY KEY (`team_id`)
 ) ENGINE=InnoDB;
 
+
 -- ----------------------------
 --  Table structure for `fixtures`
 -- ----------------------------
@@ -17,25 +19,13 @@ CREATE TABLE `fixtures_results` (
 	`phase` varchar(10) 
 	`group` int(5),
 	`datetime` datetime,
-	`team_a_id` int(8) 
-	`team_b_id` int(8)
+	`team_a_id` int(8) NOT NULL
+	`team_b_id` int(8) NOT NULL
 	`team_a_score` smallint, 
 	`team_b_score` smallint,
 	PRIMARY KEY (`fixture_id`)
 ) ENGINE=InnoDB;
 
-
--- ----------------------------
---  Table structure for `weather`
--- ----------------------------
-DROP TABLE IF EXISTS `weather`;
-CREATE TABLE `weather` (
-	`country` varchar(30),
-	`county` varchar(30),
-	`datetime` datetime,
-	`temp_cel` DOUBLE,
-	`weather` varchar(120),
-) ENGINE=InnoDB;
 
 -- ----------------------------
 --  Table structure for `league_table`
@@ -55,3 +45,15 @@ CREATE TABLE `league_table` (
 	`diff` smallint NOT NULL,  
 ) ENGINE=InnoDB;
 
+
+-- ----------------------------
+--  Table structure for `weather`
+-- ----------------------------
+DROP TABLE IF EXISTS `weather`;
+CREATE TABLE `weather` (
+	`country` varchar(30),
+	`county` varchar(30),
+	`datetime` datetime,
+	`temp_cel` DOUBLE,
+	`weather` varchar(120),
+) ENGINE=InnoDB;
